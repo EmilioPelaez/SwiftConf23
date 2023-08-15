7e35c0e497f1938fbb4f31df8b3228139d0e2369
Emilio Peláez <me@epelaez.net>
2023-08-15 11:45:25 +0200

    More Slides Content

--- SwiftConf/Slides/ChannelsBasicsSlide.swift
+++ SwiftConf/Slides/ChannelsBasicsSlide.swift
@@ -24,15 +24,12 @@ struct ChannelsBasicsSlide: Slide {
 			}
 			if phasedStateStore.after(.bindings) {
 				Element("Bindings") {
-					Callout("")
+					Callout("Wrapper for a value")
 				}
 			}
 			if phasedStateStore.after(.closures) {
 				Element("Closures") {
-					Callout("Multi-level communication")
-					Callout("Key-Value Store")
-					Callout("Defining them requires a default value")
-					Callout("Values will flow downwards until replaced")
+					Callout("")
 				}
 			}
 		}
@@ -40,23 +37,29 @@ struct ChannelsBasicsSlide: Slide {
 	}
 	
 	var script: String {
+		switch phasedStateStore.current {
+		case .arguments:
 """
 I'm going to start with the easiest and most obvious way of passing data down the view hierarchy, init parameters
 These are pretty trivial, you use them as soon as you start making an app
 They allow us to pass data down one level
 And obviously we can keep passing the data deeper and deeper but that requires explicit declarations every level
 If we want to send data back up the hierarchy, we can combine it with a binding
-
+"""
+		case .bindings:
+"""
 A binding will enable a child view to update a value and its parent will receive the new value
-This mechanism is used everywhere in SwiftUI
+This mechanism is used in many places in SwiftUI
 It's one of the ways we can use to share data between siblings and cousins
 We can pass a binding from a common ancestor to different branches of our tree
 Any updates to that binding from any of the views will be reflected in all the views that observe it
-Then we have another mechanism to pass data and events up the hierarchy, and it's a core feature of Swift, closures
-
-Just like we would with a binding, we can pass a closure to allow descendant views to execute a function defined higher in the hierarchy
-
+Then we have another mechanism to pass data and events up the hierarchy, closures
 """
+		case .closures:
+"""
+Just like we would with a binding, we can pass a closure to allow descendant views to execute a function defined higher in the hierarchy
+"""
+		}
 	}
 }
 
--- SwiftConf/Slides/ChannelsPreferencesSlide.swift
+++ SwiftConf/Slides/ChannelsPreferencesSlide.swift
@@ -9,25 +9,57 @@ import SlideKit
 import SwiftUI
 
 struct ChannelsPreferencesSlide: Slide {
+	
+	enum SlidePhasedState: Int, PhasedState {
+		case initial, preferenceKey, responderChain
+	}
+	
+	@Phase var phasedStateStore
+	
 	var body: some View {
 		HeaderSlide("Communication Channels: Preferences") {
+			Element("What are they?") {
+				Callout("\"Reverse\" Environment Values")
+			}
+			if phasedStateStore.after(.preferenceKey) {
+				Element("PreferenceKey Protocol") {
+					Callout("Only one public type conforms to it")
+					Callout("But don't use it!")
+					Callout("Almost a dozen modifiers")
+				}
+			}
+			if phasedStateStore.after(.responderChain) {
+				Element("Responder Chains") {
+					Callout("Chain of connected responders")
+					Callout("Responders may or may not handle arbitrary events")
+					Callout("If a responder can't handle an event, it's passed along")
+				}
+			}
 		}
 		.extend()
 	}
 	
 	var script: String {
+		switch phasedStateStore.current {
+		case .initial:
 """
 If we want to send values up the view hierarchy we can use Preferences
 Preferences are kind of a reverse environment value
 They also work with keys but you update it's value deeper in the hierarchy and observe it's value above
-I find these very interesting because if you look at the PreferenceKey protocol, only one type conforms to it
+"""
+		case .preferenceKey:
+"""
+I find these very intriguing because if you look at the PreferenceKey protocol, only one public type conforms to it
 If you look at the documentation it says "Don’t use this"
-
-And yet there are 11 view modifiers that you can use to interact with preferences
+And yet there are 11 different view modifiers that you can use to interact with preferences
+I believe that SwiftUI should provide more preference keys out of the box, like a scroll view offset
 The name Preferences in my opinion doesn't make it super obvious what is it that they do
 One of the most common usages for preferences I've seen is to read the size of a child view
-If we look at the available modifiers we can spot a pattern called a Responder Chain
-
+If we look at the available modifiers we can see they behave differently to environment values
+Preferences follow a pattern called a responder chain
+"""
+		case .responderChain:
+"""
 A responder chain is a design pattern where we have a number of connected objects
 Each one of this objects is called a responder because it has the potential to respond to an arbitrary event
 They way we use it is by giving an event to one of the responders of the chain
@@ -37,9 +69,11 @@ If it cannot, the event is passed to the next responder, who does the same thing
 This goes on until the event is consumed or it reaches the end
 Depending on the implementation, it may fail silently or throw an error
 Responders are not limited to just responding or not, they can also modify the event or send a different one
+
 The responder chain is one of my favorite design patterns because it's very flexible and modular
 I asked myself how can I build a responder chain that works not just with values but with events?
 """
+		}
 	}
 }
 
